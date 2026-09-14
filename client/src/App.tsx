c44362b6e8049cc1bd4f29a97b8d770f4650780f
hsdx8tx5bj-wq <2.40195199e+08+hsdx8tx5bj-wq@users.noreply.github.com>
2026-09-14 09:51:27 +0000

    Serve homepage directly on GitHub Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,11 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/NotFound";
-import { Route, Router as WouterRouter, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 
 function Router() {
-  // make sure to consider if you need authentication for certain routes
-  return (
-    <WouterRouter base="/petroleum-college-cms">
-      <Switch>
-        <Route path={"/"} component={Home} />
-        <Route path={"/404"} component={NotFound} />
-        {/* Final fallback route */}
-        <Route component={NotFound} />
-      </Switch>
-    </WouterRouter>
-  );
+  return <Home />;
 }
 
 // NOTE: About Theme
